f617e80c682622522d3347a7f6b12bbefa2544ee
minoru-github <kurami0621@yahoo.co.jp>
2023-02-14 23:19:17 +0900

    feat: copy_files

--- file-handling/src/lib.rs
+++ file-handling/src/lib.rs
@@ -1,17 +1,18 @@
 use regex::Regex;
 use std::fs;
 use std::io;
+use std::path::Path;
+use std::path::PathBuf;
 
 enum Kind {
     Dir,
     File,
 }
 
-fn retrive_name_vec(path: String, kind: Kind) -> io::Result<Vec<String>> {
+fn retrive_name_vec(path: &Path, kind: Kind) -> io::Result<Vec<PathBuf>> {
     let mut names = vec![];
 
     let entries = fs::read_dir(path)?;
-
     for entry in entries {
         let entry = entry?;
         let flag = match kind {
@@ -19,29 +20,38 @@ fn retrive_name_vec(path: String, kind: Kind) -> io::Result<Vec<String>> {
             Kind::File => entry.file_type()?.is_file(),
         };
         if flag {
-            let name = entry.file_name().into_string().unwrap();
-            names.push(name);
+            let name = PathBuf::from(entry.file_name());
+            names.push(name.clone());
         }
     }
 
     Ok(names)
 }
 
-pub fn retrive_folder_name_vec(path: String) -> io::Result<Vec<String>> {
+pub fn retrive_folder_name_vec(path: &Path) -> io::Result<Vec<PathBuf>> {
     retrive_name_vec(path, Kind::Dir)
 }
 
-pub fn retrive_file_name_vec(path: String) -> io::Result<Vec<String>> {
+pub fn retrive_file_name_vec(path: &Path) -> io::Result<Vec<PathBuf>> {
     retrive_name_vec(path, Kind::File)
 }
 
-pub fn retrive_name_vec_with_pattern_match(names: Vec<String>, pattern: &str) -> Vec<String> {
+pub fn retrive_name_vec_with_pattern_match(names: Vec<PathBuf>, pattern: &str) -> Vec<PathBuf> {
     //! regular expression
     let regex = Regex::new(pattern).unwrap();
+
     names
         .into_iter()
-        .filter(|name| regex.is_match(name.as_str()))
-        .collect::<Vec<String>>()
+        .filter(|name| regex.is_match(name.file_name().unwrap().to_str().unwrap()))
+        .collect::<Vec<PathBuf>>()
+}
+
+pub fn copy_files(path_vec: Vec<PathBuf>, dist: &Path) -> io::Result<()> {
+    for path in path_vec {
+        let to = dist.join(path.file_name().unwrap());
+        fs::copy(path, to)?;
+    }
+    Ok(())
 }
 
 #[cfg(test)]
@@ -50,10 +60,16 @@ mod tests {
 
     #[test]
     fn test_retrive_folder_names() {
-        let path = "./test-data".to_string();
+        let ans = vec!["0001", "0002", "0003", "hoge"];
+        let ans = ans
+            .iter()
+            .map(|&a| PathBuf::from(a))
+            .collect::<Vec<PathBuf>>();
+
+        let path = Path::new("./test-data");
         let names = retrive_folder_name_vec(path);
         if let Ok(names) = names {
-            assert_eq!(vec!["0001", "0002", "0003", "hoge"], names);
+            assert_eq!(ans, names);
         } else {
             assert!(false);
         }
@@ -61,10 +77,10 @@ mod tests {
 
     #[test]
     fn test_retrive_file_names() {
-        let path = ".".to_string();
+        let path = Path::new(".");
         let names = retrive_file_name_vec(path);
         if let Ok(names) = names {
-            assert_eq!(vec!["Cargo.toml"], names);
+            assert_eq!(vec![PathBuf::from("Cargo.toml")], names);
         } else {
             assert!(false);
         }
@@ -72,14 +88,57 @@ mod tests {
 
     #[test]
     fn test_retrive_names_with_pattern_match() {
-        let path = "./test-data".to_string();
+        let ans = vec!["0001", "0002", "0003"];
+        let ans = ans
+            .iter()
+            .map(|&a| PathBuf::from(a))
+            .collect::<Vec<PathBuf>>();
+
+        let path = Path::new("./test-data");
         let names = retrive_folder_name_vec(path);
         if let Ok(names) = names {
             let regex = r"\d{4}";
             let names = retrive_name_vec_with_pattern_match(names, regex);
-            assert_eq!(vec!["0001", "0002", "0003"], names);
+            assert_eq!(ans, names);
         } else {
             assert!(false);
         }
+    }
+
+    #[test]
+    fn test_copy_files() {
+        let ans = vec!["1.txt", "2.txt", "Cargo.toml"];
+        let ans = ans
+            .iter()
+            .map(|&a| PathBuf::from(a))
+            .collect::<Vec<PathBuf>>();
+
+        let files = vec!["./test-data/1.txt", "./test-data/2.txt"];
+        let path_vec = files
+            .iter()
+            .map(|&f| PathBuf::from(f))
+            .collect::<Vec<PathBuf>>();
+
+        path_vec.iter().for_each(|path| {
+            fs::File::create(path).ok();
+        });
+
+        let dist = Path::new(".");
+        copy_files(path_vec.clone(), dist).ok();
+
+        let names = retrive_file_name_vec(Path::new(".")).ok().unwrap();
+        assert_eq!(ans, names);
+
+        // 後始末
+        path_vec.iter().for_each(|path| {
+            fs::remove_file(path).ok();
+        });
+
+        names
+            .iter()
+            .filter(|f| f.file_name().unwrap() != "Cargo.toml")
+            .for_each(|f| {
+                fs::remove_file(f).ok();
+            })
     }
 }
