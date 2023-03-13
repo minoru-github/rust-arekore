c6e2d1abd926c8f404003e3c6d2d139fe7af0c8d
minoru-github <kurami0621@yahoo.co.jp>
2023-03-13 22:38:09 +0900

    feat: add sort_by_single_comparison

--- trait-sort/src/lib.rs
+++ trait-sort/src/lib.rs
@@ -1,8 +1,5 @@
 #![allow(unused)]
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use std::cmp::Ordering;
 
 pub struct Node {
     id: usize,
@@ -22,6 +19,28 @@ impl Node {
     }
 }
 
+// https://doc.rust-lang.org/stable/std/cmp/trait.Ord.html#how-can-i-implement-ord
+impl PartialEq for Node {
+    fn eq(&self, other: &Self) -> bool {
+        self.score == other.score
+    }
+}
+
+impl Eq for Node {}
+// or #[derive(Eq)] to struct
+
+impl PartialOrd for Node {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.score.cmp(&other.score))
+    }
+}
+
+impl Ord for Node {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.score.cmp(&other.score)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use std::collections::BinaryHeap;
@@ -29,22 +48,27 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-
-    #[test]
-    fn test_sort() {
+    fn test_sort_by_single_comparison() {
         let nodes = [
-             Node::new(0, 10),
-             Node::new(1, -20),
-             Node::new(2, 100)
+            Node::new(0, 10),
+            Node::new(1, -20),
+            Node::new(2, 100),
+            Node::new(3, 100),
         ];
 
         let mut q = BinaryHeap::new();
         for node in nodes {
             q.push(node);
         }
+
+        let mut ans = vec![];
+        while !q.is_empty() {
+            let node = q.pop().unwrap();
+            //println!("{} {}", node.id, node.score);
+            ans.push(node.id);
+        }
+
+        let est = vec![2,3,0,1];
+        assert_eq!(ans,est);
     }
 }
