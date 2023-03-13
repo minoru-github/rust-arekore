78d7bd0092e9064a2ac1c9d45ed958dba904c228
minoru-github <kurami0621@yahoo.co.jp>
2023-03-13 13:00:29 +0900

    feat: ベース実装。NodeにOrdが実装されてないのでエラー

--- trait-sort/src/lib.rs
+++ trait-sort/src/lib.rs
@@ -1,14 +1,50 @@
+#![allow(unused)]
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+pub struct Node {
+    id: usize,
+    parent: Option<usize>,
+    childs: Vec<usize>,
+    score: i32,
+}
+
+impl Node {
+    fn new(id: usize, score: i32) -> Self {
+        Node {
+            id,
+            parent: None,
+            childs: vec![],
+            score,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use std::collections::BinaryHeap;
+
     use super::*;
 
     #[test]
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn test_sort() {
+        let nodes = [
+             Node::new(0, 10),
+             Node::new(1, -20),
+             Node::new(2, 100)
+        ];
+
+        let mut q = BinaryHeap::new();
+        for node in nodes {
+            q.push(node);
+        }
     }
 }
