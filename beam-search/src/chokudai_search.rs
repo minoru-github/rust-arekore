5c074d65e8e478b20ec0975578afbb3494fb4e1d
minoru-github <kurami0621@yahoo.co.jp>
2023-03-17 22:50:07 +0900

    test: add test

--- beam-search/src/chokudai_search.rs
+++ beam-search/src/chokudai_search.rs
@@ -4,9 +4,14 @@ use crate::state::*;
 
 use std::collections::BinaryHeap;
 
-fn chokudai_search(state: &State, beam_width: usize, beam_depth: usize, beam_number:usize) -> Option<usize>{
+fn chokudai_search(
+    state: &State,
+    beam_width: usize,
+    beam_depth: usize,
+    beam_number: usize,
+) -> Option<usize> {
     let mut beam = vec![];
-    for t in 0..beam_width+1 {
+    for t in 0..beam_depth + 1 {
         beam.push(BinaryHeap::new());
     }
     beam[0].push(state.clone());
@@ -31,16 +36,73 @@ fn chokudai_search(state: &State, beam_width: usize, beam_depth: usize, beam_num
                     if t == 0 {
                         next_state.first_action = Some(action);
                     }
-                    beam[t+1].push(next_state);
+                    beam[t + 1].push(next_state);
                 }
             }
         }
     }
 
-    for now_beam in beam {
+    // for state in beam[beam_depth].iter() {
+    //     state.debug();
+    // }
+
+    for now_beam in beam.iter().rev() {
         if !now_beam.is_empty() {
             return now_beam.peek().unwrap().first_action;
         }
     }
     None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_beam_search_w1_d1_n4() {
+        let character = Pos::new(1, 1);
+        let points = vec![vec![4, 6, 1, 3], vec![0, 0, 2, 0], vec![7, 5, 6, 6]];
+        let state = State::new(character, points);
+        let beam_width = 1;
+        let beam_depth = 1;
+        let beam_number = 4;
+        let action = chokudai_search(&state, beam_width, beam_depth, beam_number);
+        assert_eq!(action, Some(3));
+    }
+
+    #[test]
+    fn test_beam_search_w1_d2_n2() {
+        let character = Pos::new(1, 1);
+        let points = vec![vec![4, 6, 1, 3], vec![0, 0, 2, 0], vec![7, 5, 6, 6]];
+        let state = State::new(character, points);
+        let beam_width = 1;
+        let beam_depth = 2;
+        let beam_number = 2;
+        let action = chokudai_search(&state, beam_width, beam_depth, beam_number);
+        assert_eq!(action, Some(1));
+    }
+
+    #[test]
+    fn test_beam_search_w1_d3_n2() {
+        let character = Pos::new(1, 1);
+        let points = vec![vec![4, 6, 1, 3], vec![0, 0, 2, 0], vec![7, 5, 6, 6]];
+        let state = State::new(character, points);
+        let beam_width = 1;
+        let beam_depth = 3;
+        let beam_number = 2;
+        let action = chokudai_search(&state, beam_width, beam_depth, beam_number);
+        assert_eq!(action, Some(1));
+    }
+
+    #[test]
+    fn test_beam_search_w1_d4_n4() {
+        let character = Pos::new(1, 1);
+        let points = vec![vec![4, 6, 1, 3], vec![0, 0, 2, 0], vec![7, 5, 6, 6]];
+        let state = State::new(character, points);
+        let beam_width = 1;
+        let beam_depth = 4;
+        let beam_number = 2;
+        let action = chokudai_search(&state, beam_width, beam_depth, beam_number);
+        assert_eq!(action, Some(1));
+    }
 }
